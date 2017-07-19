66b9015b6b9e03c7b9373460fd8348b21e8fad28
Sergey-Akmalov <akmolgt@gmail.com>
2017-07-19 15:25:00 +0300

    add news to template

--- src/index.js
+++ src/index.js
@@ -1,18 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader'
 
-const render = Component => {
-    ReactDOM.render(
-        <AppContainer>
-            <Component />
-        </AppContainer>,
-        document.getElementById('root')
-    )
-};
+let my_news = [
+    {
+        author: 'Саша Печкин',
+        text: 'В четверг, четвёртого числа...'
+    },
+    {
+        author: 'Просто Вася',
+        text: 'Считаю, что $ должен стоить 35 рублей!'
+    },
+    {
+        author: 'Гость',
+        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    }
+];
 
-//render();
+let News = React.createClass({
+    render: function() {
+        let last_news = this.props.last_news;
+        let newsTemplate = last_news.map(function(item, index) {
+            return (
+                <div key={index}>
+                    <p className="news__author">{item.author}:</p>
+                    <p className="news__text">{item.text}</p>
+                </div>
+            )
+        });
+        return (
+            <div className="news">
+                {newsTemplate}
+            </div>
+        );
+    }
+});
 
-//if (module.hot) {
-//    module.hot.accept('./app', () => { render(Hello) })
-//}
+let Comments = React.createClass({
+    render: function() {
+        return (
+            <div className="comments">
+                Нет новостей - комментировать нечего.
+            </div>
+        );
+    }
+});
+
+let App = React.createClass({
+    render: function() {
+        return (
+            <div className="app">
+                Отображение новостей
+                <News last_news={my_news} />
+                <Comments />
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);

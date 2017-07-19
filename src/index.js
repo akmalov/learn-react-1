8f7cfcc9a4b72d51db51dfb4fcccc45a9a1faca2
Sergey-Akmalov <akmolgt@gmail.com>
2017-07-19 18:33:18 +0300

    add texts of articles

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ let my_news = [
     {
         author: 'Саша Печкин',
         text: 'В четверг, четвёртого числа...',
+        bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилма чертёж.'
     },
     {
         author: 'Просто Вася',
-        text: 'Считаю, что $ должен стоить 35 рублей!'
+        text: 'Считаю, что $ должен стоить 35 рублей!',
+        bigText: 'А евро 42!'
     },
     {
         author: 'Гость',
-        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+        bigText: 'На самом деле платно, нужно прочитать очень длинное лицензионное соглашение'
     }
 ];
 
@@ -20,17 +23,21 @@ let Article = React.createClass({
     propTypes: {
         last_news: React.PropTypes.shape({
             author: React.PropTypes.string.isRequired,
-            text: React.PropTypes.string.isRequired
+            text: React.PropTypes.string.isRequired,
+            bigText: React.PropTypes.string.isRequired
         })
     },
     render: function() {
        let author = this.props.last_news.author,
-           text = this.props.last_news.text;
+           text = this.props.last_news.text,
+           bigText = this.props.last_news.bigText;
 
        return (
-           <div className="article">
-               <p className="news__author">{author}:</p>
-               <p className="news__text">{text}</p>
+           <div className='article'>
+               <p className='news__author'>{author}:</p>
+               <p className='news__text'>{text}</p>
+               <a href="#" className='news__readmore'>Подробнее</a>
+               <p className='news__big-text'>{bigText}</p>
            </div>
        )
    }
@@ -69,7 +76,7 @@ let News = React.createClass({
 let App = React.createClass({
     render: function() {
         return (
-            <div className="app">
+            <div className='app'>
                 <h3>Новости</h3>
                 <News last_news={my_news} />
             </div>
